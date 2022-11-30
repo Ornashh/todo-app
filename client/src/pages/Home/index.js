655d5cff9e4ca80f6fc1f57d5ce885f78b42349c
Ornash <ornash1337@gmail.com>
2022-11-30 20:34:00 +0500

    styles

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,26 +2,14 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 import s from "./home.module.scss";
-import { useAppContext } from "../../context";
 
 const Home = () => {
   const { t } = useTranslation();
-  const { lang } = useAppContext();
 
   return (
     <div className={s.home_outer}>
       <div className={s.home_inner}>
-        <h1
-          className={s.title}
-          style={{
-            fontSize:
-              lang === "en"
-                ? "clamp(50px, 9vw, 90px)"
-                : "clamp(30px, 9vw, 90px)",
-          }}
-        >
-          {t("Main title")}
-        </h1>
+        <h1 className={s.title}>{t("Main title")}</h1>
       </div>
     </div>
   );
